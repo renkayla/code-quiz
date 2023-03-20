de6a0474f3b88e94af3106ca79be217b2e468d17
renkayla <116777951+renkayla@users.noreply.github.com>
2023-03-20 17:41:33 -0400

    added questions

--- assets/script.js
+++ assets/script.js
@@ -14,14 +14,59 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score   */
 
-/* Define Variables 
-Assignment code to section 
-Questions are defined in Objects
-Other variables are set */
+// Define Variables 
+//Assignment code to section 
+
+
+//Questions are defined in Objects
+var questionSource = [
+    {
+        question: "Which code will declare a JavaScript variable named myVariable and save the value 1 to it?",
+        choices: ["a. myVariable", "b. 1 = myVariable;", "c. const myVariable = 1;", "d. myVariable === 1;"],
+        answer: "c. const myVariable = 1;"
+    },
+    {
+        question: "What is preferable: defining variables in the global scope or defining variable in the block scope?",
+        choices: ["a. they are equal", "b. global scope", "c. defining in block scope", "d. defining in lateral scope"],
+        answer: "c. defining in block scope"
+    },
+    {
+        question: "What are the data types in JavaScript?"
+        choices: ["a. string, number, boolean, bignit, defined, null, symbol, object", "b. array, date, object, string, boolean, bigint, null, symbol", "c. string, number, bigint, boolean, undefined, null, symbol, object", "d. length, weight, true, false, date, let, var, const, "]
+        answer: "c. string, number, bigint, boolean, undefined, null, symbol, object"
+    },
+    {
+        question: "What is the first index of an Array?"
+        choices: ["a. 1", "b. 10 ", "c. -1", "d. 0"]
+        answer: "d. 0"
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?"
+        choices: ["a. <js>", "b. <javascript> ", "c. <script> ", "d. <scripting>"]
+        answer: "c. <script>"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?"
+        choices: ["a. <script src='xxx.js'>", "b. <script href='xxx.js'> ", "c.  <script name='xxx.js'> ", "d. <script id='xxx.js'> "]
+        answer: "a. <script src='xxx.js'>"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?"
+        choices: ["a. alertBox('Hello World');", "b. msgBox('Hello World'); ", "c. alert('Hello World');", "d. msg('Hello World'); "]
+        answer: "c. alert('Hello World');"
+    },
+    {
+        question: "How do you create a function in JavaScript?"
+        choices: ["a. function myFunction()", "b.function = myFunction() ", "c. function:myFunction() ", "d. myfunction(); "]
+        answer: "a. function myFunction()"
+    },
+]
+
+//Other variables are set 
 
 var timeLeft = document.getElementById("timer");
 
-var secondsLeft = 100;
+var secondsLeft = 75;
 
 /* Functions :
 WHEN I click the start button
